972e7caf0824e237e126389c75f9cee418a1dd4a
Heron Rodrigues <heronrs1@gmail.com>
2020-03-25 13:30:41 -0300

    creating ongs table in database

---
+++ backend/src/database/migrations/20200325132158_create_ongs.js
@@ -0,0 +1,16 @@
+// Up cria tabela no banco de dados
+exports.up = function(knex) {
+    knex.schema.createTable('ongs', function(table) {
+        table.string('id').primary();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('name').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+};
+
+// Down trata erros que possam ocorrer
+exports.down = function(knex) {
+    knex.schema.dropTable('ongs');
+};
