fd44ff36ab0d0958f96f8fff163573fa04c23e87
heronsousa <heronrs1@gmail.com>
2020-03-25 17:13:04 -0300

    creating session controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
@@ -14,5 +15,7 @@ routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/profile', ProfileController.index);
+
+routes.post('/session', SessionController.create);
 
 module.exports = routes;
