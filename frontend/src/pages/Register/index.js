ed84587f707f87a436fd8c95fcbea3f938d9245f
heronsousa <heronrs1@gmail.com>
2020-04-01 12:41:37 -0300

    styling register page

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -15,7 +15,7 @@ export default function Register() {
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude as pessoas a encontrarem casos da sua ONG.</p>
 
-                    <Link className='back-link' to="/register">
+                    <Link className='back-link' to="/">
                         <FiArrowLeft size={16} color='#E02041' />
                         Não tenho cadastro
                     </Link>
@@ -30,6 +30,8 @@ export default function Register() {
                         <input placeholder='Cidade' />
                         <input placeholder='UF' style={{width: 80}} />
                     </div>
+
+                    <button className='button' type='submit'>Cadastrar</button>
                 </form>
             </div>
         </div>
