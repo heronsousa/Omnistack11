7d0d89d893f3d16418a7fef16125bd786a3659b1
heronsousa <heronrs1@gmail.com>
2020-04-03 22:05:42 -0300

    logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory} from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 import heroes from '../../assets/heroes.png';
 import logo from '../../assets/logo.svg';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogon(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('session', { id });
+
+            localStorage.setItem('name', response.data.name);
+            localStorage.setItem('id', id);
+
+            history.push('/profile');
+        } catch (error) {
+            alert('Falha no login, tente novamente.')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logo} alt="logo"/>
 
-                <form>
+                <form onSubmit={handleLogon}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}    
+                    />
                     <button className='button' type='submit'>Entrar</button>
 
                     <Link className='back-link' to="/register">
