8c9479ed1b65b36a14d445197f271d20176bd701
heronsousa <heronrs1@gmail.com>
2020-04-05 23:08:21 -0300

    adding incident page incident list

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
 
 import logo from '../../assets/logo.png';
 import styles from './styles';
@@ -16,6 +17,33 @@ export default function Incidents() {
 
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+            <FlatList 
+                data={[1, 2, 3]}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                style={styles.incidentList}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>UnB</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha amora</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={() => {}}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name='arrow-right' color='#e02041' size={16}/>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     );
 }
