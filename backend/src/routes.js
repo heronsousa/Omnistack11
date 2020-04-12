ad3687184fbedf47d232eeb03da2d2fc9b1dee79
heronsousa <heronrs1@gmail.com>
2020-04-11 21:42:09 -0300

    validating route create ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Joi, Segments, celebrate } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -8,7 +9,17 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    // Validação dos parametros recebidos
+    [Segments.BODY]: Joi.object({
+        // Variaveis recebidas e as funções de validação
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().length(2)
+    })
+}), OngController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
